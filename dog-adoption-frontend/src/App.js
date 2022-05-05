80500e81849cd8ca2842ed6b02219ce33e1d817a
Rene Shen <rshen@box.com>
2022-05-05 14:48:26 -0700

    Co-authored-by: jinimbrancalhao <jinimbrancalhao@users.noreply.github.com> Co-authored-by: rz4life <rz4life@users.noreply.github.com>

--- dog-adoption-frontend/src/App.js
+++ dog-adoption-frontend/src/App.js
@@ -1,11 +1,26 @@
+import React, {useState } from "react";
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Homepage from "./components/homepage";
+import Singledog from "./components/singledog"
+import Alldogs from "./components/alldogs"
 function App() {
+  const [singledog, setSingledog] = useState({});
+  // setSingledog ("helloword");
+  // console.log(singledog)
   return (
     <div className="App">
-   
-      <h3>Dog Adoption</h3>
+      <Router> 
+      <Navbar />
+      <Routes >
+        <Route exact path= "/" element= { <Homepage />}/>
+        <Route exact path= "/singledog" element= { <Singledog singledog={singledog}/>}/> 
+        <Route exact path= "/alldogs" element= { <Alldogs setSingledog= {setSingledog}/>}/> 
+      </Routes>
 
+      </Router>
+   
     </div>
   );
 }
